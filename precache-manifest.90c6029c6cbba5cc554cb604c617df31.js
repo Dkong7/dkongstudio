c8cb3335f37ef500196348d0d71bcaee1d70fb90
Dkong7 <dkong_69@yahoo.com>
2022-05-05 13:16:29 -0500

    Updates

---
+++ precache-manifest.90c6029c6cbba5cc554cb604c617df31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9610d6abd808b719af2",
+    "url": "/dkongstudio/static/css/main.ba3ad356.chunk.css"
+  },
+  {
+    "revision": "b9610d6abd808b719af2",
+    "url": "/dkongstudio/static/js/main.b9610d6a.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/dkongstudio/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "881d40ac1236a97b7469",
+    "url": "/dkongstudio/static/js/2.881d40ac.chunk.js"
+  },
+  {
+    "revision": "af96f191e4603dcb9d09",
+    "url": "/dkongstudio/static/js/runtime~main.af96f191.js"
+  },
+  {
+    "revision": "76bc5d74186140fe71eb0f6b78330ce0",
+    "url": "/dkongstudio/index.html"
+  }
+];
