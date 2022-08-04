de7b1e875a669e2dafca265d8c4aa818ffba4838
unknown <dkong_69@yahoo.com>
2022-08-04 12:09:17 -0500

    Updates

---
+++ precache-manifest.3cb86728c036c0de8ff71ba1ee885675.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "78140699ae14a39446a9",
+    "url": "/dkongstudio/static/css/main.d1589fa2.chunk.css"
+  },
+  {
+    "revision": "78140699ae14a39446a9",
+    "url": "/dkongstudio/static/js/main.78140699.chunk.js"
+  },
+  {
+    "revision": "66c154a22e2b7b2b9b37",
+    "url": "/dkongstudio/static/js/1.66c154a2.chunk.js"
+  },
+  {
+    "revision": "a8774dff2c0dc90b9833",
+    "url": "/dkongstudio/static/js/2.a8774dff.chunk.js"
+  },
+  {
+    "revision": "289e5e45a08a814358d3",
+    "url": "/dkongstudio/static/js/runtime~main.289e5e45.js"
+  },
+  {
+    "revision": "cf228c34554cb93280447dcfe2321d28",
+    "url": "/dkongstudio/static/media/logo.cf228c34.png"
+  },
+  {
+    "revision": "d540fa29604ea255175ece5ba502ddfe",
+    "url": "/dkongstudio/static/media/hero.d540fa29.png"
+  },
+  {
+    "revision": "4cbfc2d78cd6ce348fa934b9d7c20090",
+    "url": "/dkongstudio/static/media/skill3.4cbfc2d7.png"
+  },
+  {
+    "revision": "24b511b8c3ad5482afd744920c2c90f5",
+    "url": "/dkongstudio/static/media/portafolio1.24b511b8.png"
+  },
+  {
+    "revision": "14daca972d9f3f05a4fdcfda3e7adf04",
+    "url": "/dkongstudio/static/media/portafolio2.14daca97.png"
+  },
+  {
+    "revision": "c0edb627ad37479b7543aa250a276f0e",
+    "url": "/dkongstudio/static/media/portafolio3.c0edb627.png"
+  },
+  {
+    "revision": "2cb1805b36d4928723f34d189b94a5e9",
+    "url": "/dkongstudio/static/media/portafolio4.2cb1805b.png"
+  },
+  {
+    "revision": "8b23db53bb9b2c26461eef455555ff87",
+    "url": "/dkongstudio/static/media/portafolio5.8b23db53.png"
+  },
+  {
+    "revision": "038412b33e176f112c8041e536d83667",
+    "url": "/dkongstudio/static/media/portafolio6.038412b3.png"
+  },
+  {
+    "revision": "961ed35c8b15a3b6a36430a52dfa77c1",
+    "url": "/dkongstudio/static/media/skillps.961ed35c.png"
+  },
+  {
+    "revision": "195a46461e71e4246780854453a7a922",
+    "url": "/dkongstudio/static/media/skillai.195a4646.png"
+  },
+  {
+    "revision": "760b2c44d809e90ffe8f5f6ff5753c29",
+    "url": "/dkongstudio/static/media/skillae.760b2c44.png"
+  },
+  {
+    "revision": "a1e4ed8ca1e591b1cc6f6d6b66d755b3",
+    "url": "/dkongstudio/static/media/skillau.a1e4ed8c.png"
+  },
+  {
+    "revision": "93a336e5098633711a85ebdf92ef3a86",
+    "url": "/dkongstudio/static/media/skillpr.93a336e5.png"
+  },
+  {
+    "revision": "8804dcb12b25d4c835628dd9562b132e",
+    "url": "/dkongstudio/static/media/skillpro.8804dcb1.png"
+  },
+  {
+    "revision": "f4595783b8a79a5e711a05d4321e3813",
+    "url": "/dkongstudio/static/media/skillunity.f4595783.png"
+  },
+  {
+    "revision": "70637588b45634cebb451b2efb778d73",
+    "url": "/dkongstudio/static/media/skillnode.70637588.png"
+  },
+  {
+    "revision": "e5f8528f50ce2528c89f4a85ddddfd1f",
+    "url": "/dkongstudio/index.html"
+  }
+];
