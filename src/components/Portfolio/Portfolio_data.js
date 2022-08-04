21870fffe990d68d97d71ba920b5196602f2b3d0
unknown <dkong_69@yahoo.com>
2022-08-04 14:28:02 -0500

    ortografia 3

--- src/components/Portfolio/Portfolio_data.js
+++ src/components/Portfolio/Portfolio_data.js
@@ -13,7 +13,7 @@ const Portfolio_data = [
     title: "Video Clip de Deconstrucción",
     imagenCard: portafolio1,
     link: 'https://www.youtube.com/watch?v=RZnlFJoWwoc',
-    descripcion: 'En esta ocasión trabajamos junto al maestro Brian Vásquez en la edición del video clip, Deconstrucción. Se sincronizaron diversas fuentes de audio y video para mostrar los momentos más relevantes mientras se grabo la canción',
+    descripcion: 'En esta ocasión trabajamos junto al maestro Brian Vásquez en la edición del video clip, Deconstrucción. Se sincronizaron diversas fuentes de audio y video para mostrar los momentos más relevantes mientras se grabó la canción',
     descripcion2: 'La intensión en la edición fue llevar el ritmo de la canción y capturar los momentos más expresivos de la interpretación'
   },
   {
@@ -34,7 +34,7 @@ const Portfolio_data = [
     title: "Diseño Web para Volkswagen Taos",
     imagenCard: portafolio3,
     link: 'https://www.youtube.com/watch?v=x4PWFpJWCLQ',
-    descripcion: 'Para esta marca realizamos el diseño de la interface web para el lanzamiento de uno de sus automóviles. Adicionalmente realizamos algunas piezas de merchandising y recordación de marca',
+    descripcion: 'Para esta marca realizamos el diseño de la interfaz web para el lanzamiento de uno de sus automóviles. Adicionalmente, realizamos algunas piezas de merchandising y recordación de marca',
     descripcion2: '',
 
   },
@@ -45,7 +45,7 @@ const Portfolio_data = [
     title: "Cortinillas Premios Coca-Cola",
     imagenCard: portafolio4,
     link: 'https://www.youtube.com/watch?v=nb8EOpwUls4',
-    descripcion: 'Para esta marca se produjeron animación personalizadas para unos premios planeados para su staff, siendo muy específicos en los logros de los trabajadores',
+    descripcion: 'Para este proyecto se produjeron animaciones personalizadas para unos premios planeados para su staff, siendo muy específicos en los logros de los trabajadores',
     descripcion2: 'La intención del cliente es que las piezas audiovisuales estuvieran contextualizadas en un ambiente medieval ',
   },
   {
@@ -55,8 +55,8 @@ const Portfolio_data = [
     title: "Animaciones para video juegos ",
     imagenCard: portafolio5,
     link: 'https://www.youtube.com/watch?v=d5ri1hCrPKo',
-    descripcion: 'Actualmente trabajamos en las animaciones principales del video juego Harmony Monster Journey, realizando desde concept arts, diseño de personajes, diseño de escenarios y animaciones de los movimientos principales del game play del personaje principal.',
-    descripcion2: 'Fundamentalmente se trabaja con animación 2D cuadro a cuadro, pero también nos apoyamos en assets 3d para mejorar la coherencia de nuestros escenarios y personajes',
+    descripcion: 'Actualmente, trabajamos en las animaciones principales del videojuego Harmony Monster Journey, realizando desde concept arts, diseño de personajes, diseño de escenarios y animaciones de los movimientos principales del gamePlay del personaje principal.',
+    descripcion2: 'Fundamentalmente, se trabaja con animación 2D cuadro a cuadro, pero también nos apoyamos en assets 3d para mejorar la coherencia de nuestros escenarios y personajes',
   },
   {
     id: 6,
@@ -65,7 +65,7 @@ const Portfolio_data = [
     title: "Camapaña Reebok RTS",
     imagenCard: portafolio6,
     link: 'https://www.youtube.com/watch?v=0vNGFC7Y8u8',
-    descripcion: 'En esta ocasión realizamos un completo diseño de campaña para promover el evento de la marca, realizamos el diseño grafico, la edición de piezas audiovisuales para redes sociales, grabación y edición de entrevista y apoyos gráficos.',
+    descripcion: 'En esta ocasión realizamos un completo diseño de campaña para promover el evento de la marca, realizamos el diseño gráfico, la edición de piezas audiovisuales para redes sociales, grabación y edición de entrevista y apoyos gráficos.',
     descripcion2: 'Nuestra intención en este caso era crear piezas flexibles para ser fácilmente adaptadas a distintos formatos, manteniendo rigurosamente los lineamientos del estilo de la marca',
   },
 
